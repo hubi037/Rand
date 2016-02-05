e93ab11e99a8ab0f17f7c5385c32e1b2eccb0fec
Hubert Scharfetter <hscharfe.mmt-b2011@fh-salzburg.ac.at>
2016-02-05 15:29:08 +0100

    meta files

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Awake () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float vertical = Input.GetAxis("vertical");
+        float horizontal = Input.GetAxis("horizontal");
+
+        transform.position = transform.position + new Vector3(horizontal, vertical);
+	}
+}
